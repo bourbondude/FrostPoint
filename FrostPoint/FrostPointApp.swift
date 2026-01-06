1c094f69c342fbc8b6af944bcc4591eb7d570163
Evan Williams <enw493@gmail.com>
2026-01-05 19:45:55 -0500

    Initial Commit

---
+++ FrostPoint/FrostPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostPointApp.swift
+//  FrostPoint
+//
+//  Created by Evan Williams on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FrostPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrostPointTests/FrostPointTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrostPointTests.swift
+//  FrostPointTests
+//
+//  Created by Evan Williams on 1/5/26.
+//
+
+import Testing
+@testable import FrostPoint
+
+struct FrostPointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
